5c1ca6e26f3673d0fde30140170fa623bbe18ed9
Beldar <beldar.cat@gmail.com>
2016-05-30 17:51:17 +0100

    Try to fix mongo url

--- dist/lib/controller.js
+++ dist/lib/controller.js
@@ -2,7 +2,7 @@
 
 var Botkit = require('botkit');
 var config = require('../config');
-var mongoUri = process.env.OPENSHIFT_MONGODB_DB_HOST ? 'mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' : config.MONGO;
+var mongoUri = process.env.OPENSHIFT_MONGODB_DB_HOST ? 'mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/alfred' : config.MONGO;
 
 console.log('===> Connecting to mongo: ', mongoUri);
 
